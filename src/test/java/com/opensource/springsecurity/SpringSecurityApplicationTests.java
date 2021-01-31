6ba5177eb4b7e1fc2ce71449aa43965c7437f150
aakash <aakash>
2021-01-31 15:53:02 +0530

    Delegating password encoder

--- src/test/java/com/opensource/springsecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/opensource/springsecurity/SpringSecurityApplicationTests.java
@@ -1,9 +1,15 @@
 package com.opensource.springsecurity;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
@@ -16,6 +22,16 @@ class SpringSecurityApplicationTests {
 		System.out.println(new BCryptPasswordEncoder().encode(rawPassword));
 		System.out.println(new Pbkdf2PasswordEncoder().encode(rawPassword));
 		System.out.println(new SCryptPasswordEncoder().encode(rawPassword));
+		/*
+		 * Map<String, Integer> map = Stream.of(new Object[][] { { "data1", 1 }, {
+		 * "data2", 2 }, }).collect(Collectors.toMap(data -> (String) data[0], data ->
+		 * (Integer) data[1]));
+		 */
+		Map<String, PasswordEncoder> endcoderMap = Stream
+				.of(new Object[][] { { "bcrypt", new BCryptPasswordEncoder() },
+						{ "pbkdf2", new Pbkdf2PasswordEncoder() }, { "scrypt", new SCryptPasswordEncoder() }, })
+				.collect(Collectors.toMap(data -> (String) data[0], data -> (PasswordEncoder) data[1]));
+		System.out.println(new DelegatingPasswordEncoder("bcrypt", endcoderMap).encode(rawPassword));
 	}
 
 }
