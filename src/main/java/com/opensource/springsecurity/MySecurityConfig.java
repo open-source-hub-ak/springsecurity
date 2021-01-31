07a65e13a969f8f4fe10dae800740ce59ad66b7a
aakash <aakash>
2021-01-31 14:23:16 +0530

    Security configuration using Custom Userdetail service

---
+++ src/main/java/com/opensource/springsecurity/MySecurityConfig.java
@@ -0,0 +1,41 @@
+package com.opensource.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		InMemoryUserDetailsManager userDetailsService = new  InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("tom").password(passwordEncoder.encode("pass")).authorities("read").build();
+		userDetailsService.createUser(user);
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests()
+		.antMatchers("/hello").authenticated() // hello will be authenticated
+		.anyRequest().denyAll(); //this will make sure other than hello all the other request need to be denied
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
